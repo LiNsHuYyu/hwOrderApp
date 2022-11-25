766f9dba41781c5feb298cf85410f726f6eb8612
LiNsHuYyu <mich030403@gmail.com>
2022-11-25 15:25:15 +0800

    add intent

--- app/src/main/java/com/example/hw5/MainActivity.kt
+++ app/src/main/java/com/example/hw5/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.hw5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnmaindish = findViewById<Button>(R.id.btnMainDish)
+        btnmaindish.setOnClickListener {
+            val intent=intent
+            finish()
+            startActivity(intent)
+        }
     }
 }
