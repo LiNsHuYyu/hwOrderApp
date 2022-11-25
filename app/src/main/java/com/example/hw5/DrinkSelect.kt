07ef6b19fb3a6cdfb64326768b1083866ca7476e
LiNsHuYyu <mich030403@gmail.com>
2022-11-25 15:12:54 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/hw5/DrinkSelect.kt
@@ -0,0 +1,11 @@
+package com.example.hw5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrinkSelect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drink_select)
+    }
+}
